6068fd7f899a2e30f2ef239cdc6bd7bc82a2bc9f
guntursatya25 <guntursatya812@gmail.com>
2022-11-24 11:55:04 +0700

    Initial commit

---
+++ app/src/main/java/com/guntursatya/perpustakaan/MainActivity.java
@@ -0,0 +1,91 @@
+package com.guntursatya.perpustakaan;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.guntursatya.perpustakaan.adapters.CustomCursorAdapter;
+import com.guntursatya.perpustakaan.adapters.DBHelper;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    ListView Is;
+    DBHelper dbHelper;
+    Context context;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, AddActivity.class));
+            }
+        });
+
+        dbHelper = new DBHelper(this);
+        Is = (ListView)findViewById(R.id.list_pinjam);
+        Is.setOnItemClickListener(this);
+
+        setupListView();
+    }
+
+    private void setupListView() {
+        Cursor cursor = dbHelper.allData();
+        CustomCursorAdapter customCursorAdapter = new CustomCursorAdapter(this, cursor, 1);
+        Is.setAdapter(customCursorAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int i, long I) {
+        TextView getID = (TextView)view.findViewById(R.id.listID);
+        final long id = Long.parseLong(getID.getText().toString());
+        Cursor cur = dbHelper.oneData(id);
+        cur.moveToFirst();
+
+        Intent idpinjam = new Intent(MainActivity.this, AddActivity.class);
+        idpinjam.putExtra(DBHelper.row_id, id);
+        startActivity(idpinjam);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setupListView();
+    }
+}
